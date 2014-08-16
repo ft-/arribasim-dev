446b71975b3904f804187882ec499d0c997ecaef
ft <ft@noemail>
2014-08-16 10:58:57 +0200

    Revert "integrating essence of opensim-git 91e1aaa5d41c29fa17609c69ca8bc2a8017dc161 and e0c6bfa81e842f4ba1534882c6d76a3e6e94d68b:"
    This reverts commit bf81ef111e3289c07c739a772c35782b109e6992.
    
    That patch removes unforeseen parts of the avatar data transfer

--- OpenSim/Region/ClientStack/Linden/UDP/LLUDPServer.cs
+++ OpenSim/Region/ClientStack/Linden/UDP/LLUDPServer.cs
@@ -1719,7 +1719,7 @@ namespace OpenSim.Region.ClientStack.LindenUDP
                         AgentCircuitData aCircuit = m_scene.AuthenticateHandler.GetAgentCircuitData(uccp.CircuitCode.Code);
                         bool tp = (aCircuit.teleportFlags > 0);
                         // Let's delay this for TP agents, otherwise the viewer doesn't know where to get resources from
-                        if (!tp && !client.SceneAgent.SentInitialDataToClient)
+                        if (!tp)
                             client.SceneAgent.SendInitialDataToMe();
                     }
                 }
