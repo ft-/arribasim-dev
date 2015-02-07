1f2a7700ef6ec424c810757abe1a67e879db873b
ft <ft@noemail>
2015-02-07 22:13:42 +0100

    replaced old GetMeshHandler with code derived from GetTextureHandler added GetMesh2 capability

--- OpenSim/Capabilities/Handlers/GetMesh/GetMeshServerConnector.cs
+++ OpenSim/Capabilities/Handlers/GetMesh/GetMeshServerConnector.cs
@@ -62,15 +62,15 @@ namespace OpenSim.Capabilities.Handlers
             if (m_AssetService == null)
                 throw new Exception(String.Format("Failed to load AssetService from {0}; config is {1}", assetService, m_ConfigName));
 
-            GetMeshHandler gmeshHandler = new GetMeshHandler(m_AssetService);
-            IRequestHandler reqHandler
-                = new RestHTTPHandler(
-                    "GET",
-                    "/CAPS/" + UUID.Random(),
-                    httpMethod => gmeshHandler.ProcessGetMesh(httpMethod, UUID.Zero, null),
-                    "GetMesh",
-                    null);
-            server.AddStreamHandler(reqHandler);
+            string rurl = serverConfig.GetString("GetMeshRedirectURL");
+
+            server.AddStreamHandler(
+                new GetTextureHandler("/CAPS/GetMesh/" /*+ UUID.Random() */, m_AssetService, "GetMesh", null, rurl));
+
+            rurl = serverConfig.GetString("GetMesh2RedirectURL");
+
+            server.AddStreamHandler(
+                new GetTextureHandler("/CAPS/GetMesh2/" /*+ UUID.Random() */, m_AssetService, "GetMesh2", null, rurl));
         }
     }
 }
