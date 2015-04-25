033f200298a7f2c1a5861a7463ff5c71049a0203
AliciaRaven <alicia@spellscape.co.uk>
2015-04-25 06:39:20 +0200

    integrating opensim-git cde4e7ec865b1e6c933fad0f6b14e6df55123f0c:
    Fix the click to buy prim dialog not being displayed with alpha viewers after recent changes to viewer code by LL.
    
    Recent viewer changes by linden lab now require more information when requesting prim tooltip data. The object properties must now be sent when the viewer requests the object family properties used to display the tool tip. Thanks to Liru for finding the viewer commit that broke this feature in OS.
    
    Signed-off-by: ft <ft@noemail>

--- OpenSim/Region/ClientStack/Linden/UDP/LLClientView.cs
+++ OpenSim/Region/ClientStack/Linden/UDP/LLClientView.cs
@@ -4350,7 +4350,7 @@ namespace OpenSim.Region.ClientStack.LindenUDP
         {
             uint priority = 0;  // time based ordering only
             lock (m_entityProps.SyncRoot)
-                m_entityProps.Enqueue(priority, new ObjectPropertyUpdate(entity,requestFlags,true,false));
+                m_entityProps.Enqueue(priority, new ObjectPropertyUpdate(entity,requestFlags,true,true));
         }
 
         private void ResendPropertyUpdate(ObjectPropertyUpdate update)
