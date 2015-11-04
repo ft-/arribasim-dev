df9418a946cf7f8c4ba22f02fdfe46bc46ed25be
ft <ft@noemail>
2015-11-04 21:32:28 +0100

    added persist-uuids on remote admin logic

--- OpenSim/ApplicationPlugins/RemoteController/RemoteAdminPlugin.cs
+++ OpenSim/ApplicationPlugins/RemoteController/RemoteAdminPlugin.cs
@@ -1601,6 +1601,7 @@ namespace OpenSim.ApplicationPlugins.RemoteController
                     
                 bool mergeOar = false;
                 bool skipAssets = false;
+                bool persistUuids = false;
 
                 if ((string)requestData["merge"] == "true")
                 {
@@ -1610,11 +1611,16 @@ namespace OpenSim.ApplicationPlugins.RemoteController
                 {
                     skipAssets = true;
                 }
+                if ((string)requestData["persist-uuids"] == "true")
+                {
+                    persistUuids = true;
+                }
 
                 IRegionArchiverModule archiver = scene.RequestModuleInterface<IRegionArchiverModule>();
                 Dictionary<string, object> archiveOptions = new Dictionary<string,object>();
                 if (mergeOar) archiveOptions.Add("merge", null);
                 if (skipAssets) archiveOptions.Add("skipAssets", null);
+                if (persistUuids) archiveOptions.Add("persist-uuids", null);
                 if (archiver != null)
                     archiver.DearchiveRegion(filename, Guid.Empty, archiveOptions);
                 else
